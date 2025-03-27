42ec8d3074ba5c1cf904139081f741c0b8071536
RuboDev <rubololer@gmail.com>
2025-03-27 09:39:47 +0100

    modificado: css de menu y overlay para comportamiento correcto en rupturas

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
     const menu = document.querySelector(".menu");
+    const overlay = document.querySelector(".overlay");
 
     menuToggle.addEventListener("click", function() {
         menu.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+
+    // Cerrar menú al hacer clic en el fondo borroso
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('active');
+        overlay.classList.remove('active');
     });
 });

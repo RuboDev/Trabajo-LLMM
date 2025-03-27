9c688eb6d5d0e2abf9edda5345a82a2e4e884c5d
RuboDev <rubololer@gmail.com>
2025-03-27 13:35:16 +0100

    added: boton de cierre

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
     const menu = document.querySelector(".menu");
     const overlay = document.querySelector(".overlay");
+    const closeIcon = document.querySelector(".close-menu");
 
     menuToggle.addEventListener("click", function() {
         menu.classList.toggle("active");
         overlay.classList.toggle("active");
+    });
+
+    // Función para cerrar el menú al hacer clic en la "X"
+    closeIcon.addEventListener("click", () => {
+        menu.classList.remove("active");
+        overlay.classList.remove('active');
     });
 
     // Cerrar menú al hacer clic en el fondo borroso
